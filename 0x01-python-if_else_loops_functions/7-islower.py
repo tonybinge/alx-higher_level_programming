1ccb7b27c459b4dc095247ee93a963755cac91da
root <ochubatonyc@gmail.com>
2022-11-29 05:32:33 +0000

    Update 3

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,9 @@
-uthor - Bamidele Adefolaju
+#!/usr/bin/python3
+# Author - Bamidele Adefolaju
 
 def islower(c):
-        """Function checks for lowercase characters."""
-        if ord(c) >= 97 and ord(c) <= 122:
-            return True
-        else:
-            return False
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
